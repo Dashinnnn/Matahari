e1534bbd3b7328a291e9160621f2132ddf95901a
Dashinnnn <yagodamaratas28@gmail.com>
2025-04-28 00:18:47 +0800

    fix axios.js

--- search-engine/src/boot/axios.js
+++ search-engine/src/boot/axios.js
@@ -1,8 +1,11 @@
+// src/boot/axios.js
 import { boot } from "quasar/wrappers";
 import axios from "axios";
 
+console.log('VUE_APP_API_URL:', process.env.VUE_APP_API_URL); // Debug
+
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL, 
+  baseURL: process.env.VUE_APP_API_URL,
   timeout: 5000,
 });
 
