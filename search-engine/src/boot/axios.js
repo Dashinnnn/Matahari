0c345b7d9e9cd8c234357bf82201ba9521da5a54
Dashinnnn <yagodamaratas28@gmail.com>
2025-04-28 00:07:25 +0800

    Fixed axios.js

--- search-engine/src/boot/axios.js
+++ search-engine/src/boot/axios.js
@@ -1,14 +1,14 @@
 import { boot } from "quasar/wrappers";
-     import axios from "axios";
+import axios from "axios";
 
-     const api = axios.create({
-       baseURL: process.env.VUE_APP_API_URL || "https://your-backend-placeholder.com/api",
-       timeout: 5000,
-     });
+const api = axios.create({
+  baseURL: process.env.VUE_APP_API_URL, 
+  timeout: 5000,
+});
 
-     export default boot(({ app }) => {
-       app.config.globalProperties.$axios = axios;
-       app.config.globalProperties.$api = api;
-     });
+export default boot(({ app }) => {
+  app.config.globalProperties.$axios = axios;
+  app.config.globalProperties.$api = api;
+});
 
-     export { api };
+export { api };
